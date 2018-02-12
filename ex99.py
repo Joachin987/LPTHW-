b77fc0c2917844c427730a13107e809e308bdb87
Robinson Joachin <35608152+Joachin987@users.noreply.github.com>
2018-02-12 06:39:52 -0500

    Add files via upload

---
+++ ex99.py
@@ -0,0 +1,9 @@
+print "Hello my name is Robinson"
+print "I'm getting better!"
+print "what type of car do you want?",
+car = raw_input ()
+print "What color is it?",
+color = raw_input ()
+print "Is it 5speed or Automatic?"
+
+print "So, your %r is %r and %r very nice you must meet a lot of women." % (car,color)
